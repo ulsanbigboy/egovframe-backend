51ec5ecfe735cce4ca5e6288876ade3bb1ac0de3
울산빅보이 <103149528+ulsanbigboy@users.noreply.github.com>
2022-06-01 16:53:25 +0900

    테이블정의서추가

--- src/main/java/egovframework/com/config/EgovConfigAppIdGen.java
+++ src/main/java/egovframework/com/config/EgovConfigAppIdGen.java
@@ -6,7 +6,7 @@
  * 서브 시스템 : 
  * 일       자 : 2022.05.01
  * 개 발 환 경 : JDK1.7.0_79, RESIN-3.1.9
- * 주 요 내 용 : ■ 공통 >  로그 출력
+ * 주 요 내 용 : ■ IdGeneration 설정
  ********+*********+*********+*********+*********+*********+*********+*********/
 
 /*
@@ -27,7 +27,7 @@ import org.egovframe.rte.fdl.idgnr.impl.strategy.EgovIdGnrStrategyImpl;
 
 
 /**
- * <p>■공통 >  로그 출력</p>
+ * <p>■IdGeneration 설정</p>
  * <p>COPYRIGHT: Copyright (c) 2003</p>
  * <p>COMPANY: (LTD)KYOBOBOOK</p>
  * <DL>
@@ -43,46 +43,34 @@ import org.egovframe.rte.fdl.idgnr.impl.strategy.EgovIdGnrStrategyImpl;
  * @version  1.0
  * @since    1.0
  */
-/**
- * @ClassName : EgovConfigAppIdGen.java
- * @Description : IdGeneration 설정
- *
- * @author : 윤주호
- * @since  : 2021. 7. 20
- * @version : 1.0
- *
- * <pre>
- * << 개정이력(Modification Information) >>
- *
- *   수정일              수정자               수정내용
- *  -------------  ------------   ---------------------
- *   2021. 7. 20    윤주호               최초 생성
- * </pre>
- *
- */
 @Configuration
 public class EgovConfigAppIdGen {
+
+
+	/**
+	 * ■함수 시작 로그 출력
+	 * =================================
+	 */
 	@Autowired
 	@Qualifier("dataSource")
 	DataSource dataSource;
 
+
+	/**
+	 * ■함수 시작 로그 출력
+	 * =================================
+	 */
 	@Autowired
 	@Qualifier("egovDataSource")
 	DataSource egovDataSource;
 
-	// 구현 방법 1:
 
-	
 	/**
-	 * ■함수 시작 로그 출력
+	 * ■첨부파일 ID Generation  Config
 	 * =================================
 	 * @param logger
 	 * @param msg
 	 * @param req
-	 */
-	/**
-	 * 첨부파일 ID Generation  Config
-	 * @return
 	 */
 	@Bean(destroyMethod = "destroy")
 	public EgovTableIdGnrServiceImpl egovFileIdGnrService() {
